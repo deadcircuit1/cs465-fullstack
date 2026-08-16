f29bc3dc19534050935f4dbcc1cce493e7a86ddb
Travis LeBlanc <travis.leblanc1@snhu.edu>
2026-08-16 03:13:39 -0500

    Module 7 completed baseline

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -4,6 +4,7 @@ import { trips } from '../data/trips';
 import { TripCardComponent } from '../trip-card/trip-card.component';
 import { Trip } from '../models/trip';
 import { TripDataService } from '../services/trip-data.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 import { Router } from '@angular/router';
 
@@ -21,8 +22,14 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
-    private router: Router) {
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
       console.log('trip-listing constructor');
+  }
+
+  public isLoggedIn() {
+    return this.authenticationService.isLoggedIn();
   }
 
   public addTrip(): void {
